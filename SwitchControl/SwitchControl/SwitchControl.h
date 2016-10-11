b13c0c25f4c796bf78a399caa4df90aa11cff014
Rudolf Adamkovič <salutis@me.com>
2016-10-11 16:31:56 +0200

    Initial commit

---
+++ SwitchControl/SwitchControl/SwitchControl.h
@@ -0,0 +1,19 @@
+//
+//  SwitchControl.h
+//  SwitchControl
+//
+//  Created by Rudolf Adamkovic on 11.10.2016.
+//  Copyright © 2016 TBD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwitchControl.
+FOUNDATION_EXPORT double SwitchControlVersionNumber;
+
+//! Project version string for SwitchControl.
+FOUNDATION_EXPORT const unsigned char SwitchControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwitchControl/PublicHeader.h>
+
+
